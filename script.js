c64546f2e46ea54b38230151a94c98c095015332
Manikandan <86512073+trythebest@users.noreply.github.com>
2022-04-12 14:55:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+document.getElementById("boxsearch").addEventListener("keyup", filtersearch);
+
+
+
+   
+            var table = document.createElement("table");
+            table.className = "table";
+            //head code
+            var thead = document.createElement("thead");
+            thead.className = "thead-dark";
+
+            var tr = document.createElement("tr");
+
+            var th1 = createtrth("th", "Books");
+            var th2 = createtrth("th", "ISBN");
+            var th3 = createtrth("th", "No.pages");
+            var th4 = createtrth("th", "Authors");
+            var th5 = createtrth("th", "Publisher");
+            var th6 = createtrth("th", "Released");
+
+            tr.append(th1, th2, th3, th4, th5, th6);
+            thead.append(tr);
+
+            //body of the table code
+            var tbody = document.createElement("tbody");
+            var tr = document.createElement("tr");
+
+            async function foo() {
+                try {
+                    let res = await fetch("https://anapioficeandfire.com/api/books");
+                    let res1 = await res.json();
+                    console.log(res1)
+                    for (var i = 0; i <= res1.length; i++) {
+                        tbody.append(td(res1[i].name, res1[i].isbn, res1[i].numberOfPages, res1[i].authors, res1[i].publisher, res1[i].released));
+                    }
+
+                } catch (error) {
+                    console.log(error)
+                }
+
+            }
+            //creating tables using DOM
+
+
+
+            tbody.append(tr);
+
+            table.append(thead, tbody);
+            document.body.append(table);
+
+            function td(cname, cisbn, cnumberOfPages, cauthors, cpublisher, creleased) {
+
+                var bok = createtrth("td", cname);
+                var is = createtrth("td", cisbn);
+                var no = createtrth("td", cnumberOfPages);
+                var aut = createtrth("td", cauthors);
+                var pub = createtrth("td", cpublisher);
+                var rel = createtrth("td", creleased);
+
+                var tr = document.createElement("tr");
+                tr.append(bok, is, no, aut, pub, rel);
+                return tr;
+
+            }
+
+
+
+
+            function createtrth(element, values) {
+                var ele = document.createElement(element);
+                ele.innerHTML = values;
+                return ele;
+
+            }
+            foo();
